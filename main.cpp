2efe132475cc8746345e213737e66754f046bb93
Sumaiya Tasnim <90747318+SumaiyaTasnim000@users.noreply.github.com>
2024-02-17 15:44:23 +0600

    Different shapes drawn on four axes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <windows.h>
 #include <GL/glut.h>
 
-void display() {
-   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-   glClear(GL_COLOR_BUFFER_BIT);
 
 
-   glBegin(GL_LINES);
+
+void mydisplay() {
+	glClearColor(1.0,1.0,1.0,0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(5.0);
+
+glBegin(GL_LINES);
 
     glColor3f(0.0, 0.0, 0.0);
     glVertex2f(1.0,0.0);
@@ -15,50 +18,64 @@ void display() {
     glVertex2f(0.0,-1.0);
     glEnd();
 
-glBegin(GL_QUADS);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glVertex2f(0.2,0.6);
-      glVertex2f(0.2,0.8);
-      glVertex2f(0.6,0.8);
-      glVertex2f(0.6,0.6);
-      glEnd();
 
-glBegin(GL_QUADS);
-      glColor3f(1.0f, 0.0f, 0.0f);
-      glVertex2f(0.2,0.4);
-      glVertex2f(0.2,0.6);
-      glVertex2f(0.6,0.6);
-      glVertex2f(0.6,0.4);
-      glEnd();
+glBegin(GL_LINES);
 
-glBegin(GL_QUADS);
-      glColor3f(1.0f, 1.0f, 0.0f);
-      glVertex2f(0.2,0.2);
-      glVertex2f(0.2,0.4);
-      glVertex2f(0.6,0.4);
-      glVertex2f(0.6,0.2);
-      glEnd();
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex2f(-0.8,0.2);
+    glVertex2f(-0.7,0.8);
+    glVertex2f(-0.7,0.8);
+    glVertex2f(-0.5,0.5);
+    glVertex2f(-0.5,0.5);
+    glVertex2f(-0.3,0.8);
+    glVertex2f(-0.3,0.8);
+    glVertex2f(-0.2,0.2);
+    glEnd();
+
+
+glBegin(GL_LINES);
+
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(0.3,0.5);
+    glVertex2f(0.5,0.8);
+    glVertex2f(0.5,0.8);
+    glVertex2f(0.7,0.5);
+    glVertex2f(0.7,0.5);
+    glVertex2f(0.5,0.2);
+    glVertex2f(0.5,0.2);
+    glVertex2f(0.3,0.5);
+    glEnd();
 
 glBegin(GL_LINES);
 
     glColor3f(0.0, 0.0, 0.0);
-    glVertex2f(0.2,0.9);
-    glVertex2f(0.2,0.0);
-
+    glVertex2f(-0.7,-0.5);
+    glVertex2f(-0.2,-0.2);
     glEnd();
 
+glBegin(GL_LINES);
 
-   glFlush();
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2f(0.2,-0.5);
+    glVertex2f(0.5,-0.2);
+    glVertex2f(0.5,-0.2);
+    glVertex2f(0.7,-0.5);
+    glVertex2f(0.7,-0.5);
+    glVertex2f(0.2,-0.5);
+    glEnd();
+
+glFlush();
+
+
 }
 
 
 int main(int argc, char** argv) {
-   glutInit(&argc, argv);
-   glutCreateWindow("German Flag!!");
-   glutInitWindowSize(320, 320);
-   glutInitWindowPosition(50, 50);
-   glutDisplayFunc(display);
-   glutMainLoop();
-   return 0;
+	glutInit(&argc, argv);
+	glutCreateWindow("First Lab");
+	glutInitWindowSize(320, 320);
+	glutDisplayFunc(mydisplay);
+	glutMainLoop();
 }
+
 
